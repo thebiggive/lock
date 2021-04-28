84604e9072d606f8a37840c4286140bcb21d108b
Noel Light-Hilary <noel@noellh.com>
2021-04-28 10:24:04 +0100

    Expand `LockConflictedException` docs
    The goal of this is to reduce the chance of bad assumptions about lock acquisition behaviour

--- Exception/LockConflictedException.php
+++ Exception/LockConflictedException.php
@@ -11,8 +11,14 @@
 
 namespace Symfony\Component\Lock\Exception;
 
+use Symfony\Component\Lock\Lock;
+
 /**
  * LockConflictedException is thrown when a lock is acquired by someone else.
+ *
+ * In non-blocking mode it is caught by acquire() and acquireRead().
+ * @see Lock::acquire()
+ * @see Lock::acquireRead()
  *
  * @author Jérémy Derussé <jeremy@derusse.com>
  */
